7f51e95c5004706664be96f25787668d910496b3
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-10-12 15:54:59 -0500

    Experimento con for loop

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
-let miPelota;
-let frutas = ["manzana", "pera", "pina****56"];
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  miPelota = new Pelota();
+  noLoop();
 }
 
 function draw() {
-  background(255);
-  circle(mouseX, mouseY, 20);
-  miPelota.update(300);
-  miPelota.display();
+  background(255, 150, 210);
+  noStroke();
+  let anchura = 20;
+  let gapX = 10;
+  let gapY = 10;
+
+  for (let x = 10; x < windowWidth; x += anchura + gapX) {
+    for (let y = 10; y < windowHeight; y += anchura + gapY) {
+      fill(47, 255, 255);
+      square(x, y, anchura);
+    }
+  }
 }
